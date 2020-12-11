141aa1d043cc7f62b57781006bca1b7973ef2d8e
Josh Field <josh.t.field@bigpond.com>
2020-12-11 09:47:37 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+//axios replaces fetch //did this for Ellie
+import axios from 'axios';
 
 function App() {
+  //inputs onChange event update the set field
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const[data, setData] = useState("");
+
+  const register = () => {
+      axios({
+          method: "POST",
+          data: {
+              username: registerUsername,
+              password: registerPassword,
+          },
+          withCredentials: true,
+          url: "http://localhost:3001/register",
+      }).then((res) => console.log(res));
+  };
+  const login = () => {
+      axios({
+          method: "POST",
+          data: {
+              username: loginUsername,
+              password: loginPassword,
+          },
+          withCredentials: true,
+          url: "http://localhost:3001/login",
+      }).then((res) => console.log(res.data));
+  };
+  const getUser = () => {
+      axios({
+          method: "GET",
+          withCredentials: true,
+          url: "http://localhost:3001/getUser",
+      }).then((res) => {
+              setData(res.data)
+              console.log(res.data)
+          });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Register</h1>
+        <input placeholder='username' onChange={e => setRegisterUsername(e.target.value)}/>
+        <input placeholder='password' onChange={e => setRegisterPassword(e.target.value)}/>
+        <button onClick={register}>Submit</button>
+      </div>
+      <div>
+        <h1>Login</h1>
+        <input placeholder='username' onChange={e => setLoginUsername(e.target.value)}/>
+        <input placeholder='password' onChange={e => setLoginPassword(e.target.value)}/>
+        <button onClick={login}>Submit</button>
+      </div>
+      <div>
+        <h1>Get User</h1>
+        <button onClick={getUser}>Submit</button>
+          {data ? <h1>Welcome back {data.username}</h1> : null}
+      </div>
     </div>
   );
 }
